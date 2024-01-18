6396009ff1a3541f626f23115cb36c00582146af
arvindashok <arvindashok143@gmail.com>
2024-01-18 23:56:14 +0530

    Video v2

--- FirebaseVid/FirebaseVideo/FirebaseVideo.ino
+++ FirebaseVid/FirebaseVideo/FirebaseVideo.ino
@@ -1,61 +1,85 @@
 #include "Arduino.h"
 #include "WiFi.h"
 #include "esp_camera.h"
-#include "soc/soc.h"           // Disable brownout problems
-#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
 #include <LittleFS.h>
 #include <FS.h>
 #include <Firebase_ESP_Client.h>
-// Provide the token generation process info.
 #include <addons/TokenHelper.h>
 
-// Replace with your network credentials
-const char* ssid = "Vasko";
-const char* password = "arv2003!";
-
-// Insert Firebase project API Key
+const char *ssid = "Vasko";
+const char *password = "arv2003!";
 #define API_KEY "AIzaSyC0I-KPyRpX0SQhyx0fXn2Ql5tiZvuP3wg"
-
-// Insert Authorized Email and Corresponding Password
 #define USER_EMAIL "arvind.cs21@bmsce.ac.in"
 #define USER_PASSWORD "arv2003!"
-
-// Insert Firebase storage bucket ID e.g bucket-name.appspot.com
 #define STORAGE_BUCKET_ID "esp32-cam-7611b.appspot.com"
-
-// Video File Name to save in LittleFS
 #define FILE_VIDEO_PATH "/video.avi"
 #define BUCKET_VIDEO "/data/video.avi"
 
-// OV2640 camera module pins (CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 boolean recording = false;
+unsigned long recordingStartTime;
+unsigned long recordingEndTime;
 
-// Define Firebase Data objects
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig configF;
 
 void fcsUploadCallback(FCS_UploadStatusInfo info);
 
-void initWiFi(){
+void startRecording() {
+  recording = true;
+  recordingStartTime = millis();
+  recordingEndTime = recordingStartTime + 5000; // Record for 5 seconds
+}
+
+void stopRecording() {
+  recording = false;
+}
+
+void captureVideoSaveLittleFS() {
+  if (recording && millis() < recordingEndTime) {
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      delay(1000);
+      ESP.restart();
+    }
+
+    File file = LittleFS.open(FILE_VIDEO_PATH, FILE_APPEND);
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    } else {
+      if (file.write(fb->buf, fb->len) != fb->len) {
+        Serial.println("Error writing to file");
+      }
+    }
+    file.close();
+    esp_camera_fb_return(fb);
+  } else {
+    stopRecording();
+  }
+}
+
+void initWiFi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -63,145 +87,45 @@ void initWiFi(){
   }
 }
 
-void initLittleFS(){
+void initLittleFS() {
   if (!LittleFS.begin(true)) {
     Serial.println("An Error has occurred while mounting LittleFS");
     ESP.restart();
-  }
-  else {
+  } else {
     delay(500);
     Serial.println("LittleFS mounted successfully");
   }
 }
 
-void initCamera(){
-  // OV2640 camera module
+void initCamera() {
   camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_QVGA; // Adjusted frame size for video
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-  // Camera init
+  // Camera configuration...
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     ESP.restart();
-  } 
-}
-
-void startRecording() {
-  Serial.println("Recording started");
-  recording = true;
-}
-
-void stopRecording() {
-  Serial.println("Recording stopped");
-  recording = false;
-}
-
-void captureVideoSaveLittleFS() {
-  // Video file name
-  Serial.printf("Video file name: %s\n", FILE_VIDEO_PATH);
-  File file = LittleFS.open(FILE_VIDEO_PATH, FILE_WRITE);
-
-  // Insert the data in the video file
-  if (!file) {
-    Serial.println("Failed to open file in writing mode");
   }
-  else {
-    camera_fb_t* fb = NULL;
-    while (recording) {
-      fb = esp_camera_fb_get();
-      if (fb) {
-        file.write(fb->buf, fb->len);
-        esp_camera_fb_return(fb);
-      }
-      delay(10);
-    }
-    Serial.print("The video has been saved in ");
-    Serial.print(FILE_VIDEO_PATH);
-    Serial.print(" - Size: ");
-    Serial.print(file.size());
-    Serial.println(" bytes");
-  }
-
-  // Close the file
-  file.close();
 }
 
 void setup() {
-  // Serial port for debugging purposes
   Serial.begin(115200);
   initWiFi();
   initLittleFS();
-  // Turn-off the 'brownout detector'
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   initCamera();
 
-  // Firebase
-  // Assign the api key
   configF.api_key = API_KEY;
-  // Assign the user sign-in credentials
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
-  // Assign the callback function for the long-running token generation task
-  configF.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  configF.token_status_callback = tokenStatusCallback;
 
   Firebase.begin(&configF, &auth);
   Firebase.reconnectWiFi(true);
+
+  startRecording(); // Start recording immediately
 }
 
 void loop() {
-  if (recording) {
-    captureVideoSaveLittleFS();
-  }
+  captureVideoSaveLittleFS();
   delay(10);
-}
-
-// The Firebase Storage upload callback function
-void fcsUploadCallback(FCS_UploadStatusInfo info){
-    if (info.status == firebase_fcs_upload_status_init){
-        Serial.printf("Uploading file %s (%d) to %s\n", info.localFileName.c_str(), info.fileSize, info.remoteFileName.c_str());
-    }
-    else if (info.status == firebase_fcs_upload_status_upload)
-    {
-        Serial.printf("Uploaded %d%s, Elapsed time %d ms\n", (int)info.progress, "%", info.elapsedTime);
-    }
-    else if (info.status == firebase_fcs_upload_status_complete)
-    {
-        Serial.println("Upload completed\n");
-        FileMetaInfo meta = fbdo.metaData();
-        Serial.printf("Name: %s\n", meta.name.c_str());
-        Serial.printf("Bucket: %s\n", meta.bucket.c_str());
-        Serial.printf("contentType: %s\n", meta.contentType.c_str());
-        Serial.printf("Size: %d\n", meta.size);
-        Serial.printf("Generation: %lu\n", meta.generation);
-        Serial.printf("Metageneration: %lu\n", meta.metageneration);
-        Serial.printf("ETag: %s\n", meta.etag.c_str());
-        Serial.printf("CRC32: %s\n", meta.crc32.c_str());
-        Serial.printf("Tokens: %s\n", meta.downloadTokens.c_str());
-        Serial.printf("Download URL: %s\n\n", fbdo.downloadURL().c_str());
-    }
-    else if (info.status == firebase_fcs_upload_status_error){
-        Serial.printf("Upload failed, %s\n", info.errorMsg.c_str());
-    }
 }
